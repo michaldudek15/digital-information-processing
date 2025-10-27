e64d54b534905b868e8e508210f0ec1f8a6968aa
Michał Dudek <michalpaweldudek@gmail.com>
2025-10-27 13:52:50 +0100

    finished

--- advanced-programming-techniques-I/4.dekoratory-i-funkcje-wyzszego-rzedu/01-powitaj.py
+++ advanced-programming-techniques-I/4.dekoratory-i-funkcje-wyzszego-rzedu/01-powitaj.py
@@ -3,3 +3,8 @@ Proszę zdefiniować funkcję powitaj(), która wypisze w konsoli tekst.
 Następnie przypisać tę funkcję do zmiennej f i wywołać ją przez f().
 """
 
+def powitaj():
+    print("elo")
+
+f = powitaj
+f()
