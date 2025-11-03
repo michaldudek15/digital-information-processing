02756f20b98ebd133a159e9e2d936a6c210aba6b
Michał Dudek <michalpaweldudek@gmail.com>
2025-11-03 17:24:46 +0100

    ready to start

---
+++ advanced-programming-techniques-I/5.obsluga-plikow-moduly-i-pakiety/05-notatki/01-main.py
@@ -0,0 +1,24 @@
+"""
+Proszę przygotować mały projekt w strukturze:
+```
+notatki/
+│
+├── data/
+│   └── notatki.json
+│
+├── core/
+│   ├── __init__.py
+│   ├── pliki.py
+│   └── notatnik.py
+│
+└── main.py
+```
+
+Wymagania:
+
+- pliki.py – funkcje do zapisu/odczytu JSON,
+- notatnik.py – funkcje dodawania, wyszukiwania i usuwania
+ notatek,
+- main.py – prosty interfejs tekstowy (menu),
+po uruchomieniu użytkownik może dodawać i przeglądać notatki.
+"""
