3eacda128863c594bdd4d7dbb3fbab7a8c362aa3
Michał Dudek <michalpaweldudek@gmail.com>
2025-11-23 22:27:22 +0100

    finished

--- information-extraction/2.lista-frekwencyjna-z-clp/lista-frekwencyjna.py
+++ information-extraction/2.lista-frekwencyjna-z-clp/lista-frekwencyjna.py
@@ -5,10 +5,7 @@ proszę stworzyć listę frekwencyjną słów w tekście dla pliku pap.txt
 
 from collections import Counter
 import re
-# from clp3 import CLP
-
-import sys
-print(sys.path)
+from clp3 import clp
 
 
 def lista_frekwencyjna(plik):
@@ -18,7 +15,19 @@ def lista_frekwencyjna(plik):
     czysty_tekst = re.sub(r"[^a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ\s]", "", tekst)
 
     slowa = czysty_tekst.split()
-    licznik = Counter(slowa)
+    #licznik = Counter(slowa)
+
+
+    slowa_znorm = []
+    for s in slowa:
+        ids = clp.rec(s)
+        if ids:
+            podstawowa_forma = clp.bform(ids[0])
+            slowa_znorm.append(podstawowa_forma)
+        else:
+            slowa_znorm.append(s)
+
+    licznik = Counter(slowa_znorm)
     
     do_usuniecia = ["w", "i", "na", "z"]
     for word in do_usuniecia:
@@ -27,15 +36,16 @@ def lista_frekwencyjna(plik):
     lista = licznik.most_common()
     return lista
 
-if __name__ == "__main__":
-    plik = "pap.txt"
-    lista = lista_frekwencyjna(plik)
-    #for slowo, liczba in lista:
-    #    print(f"{slowo}: {liczba}")
 
-    # zapisanie outputu do pliku
-    with open("output.txt", "w", encoding="utf-8") as f:
-        for slowo, liczba in lista:
-            f.write(f"{slowo}: {liczba}\n") 
-
-
+plik = "pap.txt"
+lista = lista_frekwencyjna(plik)
+    
+with open("output.txt", "w", encoding="utf-8") as f:
+    for slowo, liczba in lista:
+        ids = clp.rec(slowo)
+        if ids:
+            label = clp.label(ids[0])
+        else:
+            label = "-"
+        f.write(f"{slowo}: {liczba} | {label}\n")
+print("TEST")
